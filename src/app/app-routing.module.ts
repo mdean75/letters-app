f39682cecdee59355b25891884824dd0fc21a8bf
Michael DeAngelo <deangelomp@gmail.com>
2021-09-05 09:58:22 -0500

    added about me and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LetterFormComponent} from './components/letter-form/letter-form.component';
 import {AppComponent} from './app.component';
+import {AboutMeComponent} from './components/about-me/about-me.component';
 
 
 const routes: Routes = [
   {path: '', component: LetterFormComponent},
-  {path: 'letters', component: LetterFormComponent}
+  {path: 'letters', component: LetterFormComponent},
+  {path: 'aboutme', component: AboutMeComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,20 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {FormsModule} from '@angular/forms';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LetterFormComponent
+    LetterFormComponent,
+    NavComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +33,12 @@ import {FormsModule} from '@angular/forms';
     MatInputModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    FormsModule
+    FormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
